ea63b36c32d747806b0c34a859da8b68806c2fdc
Lucas Durbec <kd.daemon.kd@gmail.com>
2024-10-17 15:37:54 +0200

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  // API DEFAULT ROUTE
+  app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/swagger', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
